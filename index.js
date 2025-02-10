fc76ba0db363b5f3667e2e85b8fda589718bd4fb
OzzyDahlberg <ozzy.dahlberg@hotmail.com>
2025-02-10 10:36:15 +0100

    more paths added...

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(cors());
 app.use(morgan("dev"));
 
-const dbpapth = path.resolve(__dirname, "../blog.db");
+const dbpath = path.resolve(__dirname, "../blog.db");
 const db = new sqlite3.Database(dbpath);
 
 app.get("/", (req, res) => {
@@ -17,7 +17,34 @@ app.get("/", (req, res) => {
 });
 
 app.get("/posts", (req, res) => {
-    db.all("SELECT * FROM POSTS", (err, rows) => {
+    db.all("SELECT * FROM posts", (err, rows) => {
+        if (err) {
+            res.status(500).json({  error: err.message  })
+        }
+        console.log("data fetched", rows);
+        res.json(rows);
+    })
+});
+app.get("/comments", (req, res) => {
+    db.all("SELECT * FROM comments", (err, rows) => {
+        if (err) {
+            res.status(500).json({  error: err.message  })
+        }
+        console.log("data fetched", rows);
+        res.json(rows);
+    })
+});
+app.get("/users", (req, res) => {
+    db.all("SELECT * FROM users", (err, rows) => {
+        if (err) {
+            res.status(500).json({  error: err.message  })
+        }
+        console.log("data fetched", rows);
+        res.json(rows);
+    })
+});
+app.get("/users/:id", (req, res) => {
+    db.all(`SELECT name, email, id FROM users WHERE id =  ${req.params.id}`, (err, rows) => {
         if (err) {
             res.status(500).json({  error: err.message  })
         }
